c0e7ca43008342741113bb8f1968983a9bfc8f8f
shooorsa <shooorsa@gmail.com>
2025-12-30 03:10:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,20 @@ const firebaseConfig = {
     messagingSenderId: "356426973132",
     appId: "1:356426973132:web:6353917bf13a64074d6769"
 };
+
+// تشغيل الفايربيس
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
+
 const auth = firebase.auth();
 const db = firebase.firestore();
+
+// *** إضافة هامة جداً لثبات تسجيل الدخول ***
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+  .then(() => {
+      console.log("نظام الحفظ مفعّل");
+  })
+  .catch((error) => {
+      console.error("خطأ في نظام الحفظ:", error.message);
+  });
